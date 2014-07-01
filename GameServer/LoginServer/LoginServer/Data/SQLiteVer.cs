e92764bbdc7b4aca850d07857ae36781eeaa2162
MachoChen <406028609@qq.com>
2014-07-02 00:09:10 +0800

    add LOGING`

--- GameServer/LoginServer/LoginServer/Data/SQLiteVer.cs
+++ GameServer/LoginServer/LoginServer/Data/SQLiteVer.cs
@@ -8,7 +8,7 @@ namespace LoginServer
 {
     public class SQLiteVer
     {
-        public const int SQLITE_VER = 3;
+        public const int SQLITE_VER = 4;
         public const string SQLITE_PATH_URL = "ftp://qq459127484:kanni789@002.3vftp.com/MG.db";
         public int ver;
         public string url;
