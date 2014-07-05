cb4a6404cf1018d34c3a78b2bd190796477a4be3
MachoChen <406028609@qq.com>
2014-07-06 01:18:19 +0800

    add room manage

---
+++ GameServer/MarsServer/MarsServer/Room/RoomManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsServer
+{
+    public class RoomManager
+    {
+        public readonly static RoomManager instance = new RoomManager();
+
+        public void CreatRoom()
+        {
+ 
+        }
+    }
+}
